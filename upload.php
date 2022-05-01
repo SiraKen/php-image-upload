c5e966c2001af1c676fce8ec73b3e75f52db293c
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-05-02 00:13:52 +0900

    fix: write error if upload failed

--- upload.php
+++ upload.php
@@ -2,13 +2,22 @@
 // get file from form
 $file = $_FILES['img_file'];
 
+
+echo '<pre>';
 var_dump($file);
+echo '</pre>';
 
 echo '<br>';
 
-if (move_uploaded_file($file['tmp_name'], $file['name'])) {
-	echo 'File uploaded successfully';
-} else {
-	echo 'There was an error uploading the file';
-}
+// if (move_uploaded_file($file['tmp_name'], $file['name'])) {
+// 	echo 'File uploaded successfully';
+// } else {
+// 	echo 'There was an error uploading the file';
+// }
 
+try {
+	move_uploaded_file($file['tmp_name'], $file['name']);
+	echo 'File uploaded successfully';
+} catch (Exception $e) {
+	echo 'There was an error uploading the file: ' . $e->getMessage();
+}
